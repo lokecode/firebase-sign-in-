b1749124c9400f82eac4ac74bc633ca10fd3af04
lokecode <zinck65@gmail.com>
2021-02-26 08:48:37 +0100

    first commit

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,38 @@
+import 'package:firebase/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SingIn extends StatefulWidget {
+  @override
+  _SingInState createState() => _SingInState();
+}
+
+class _SingInState extends State<SingIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to brew crew'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
+        child: RaisedButton(
+          child: Text('Sign in anon'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error signing in');
+            } else {
+              print('signed in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
